41cab7c08800680cc133282831a172a8518d3d55
yasiupl <contact@yasiu.pl>
2019-01-04 20:15:08 +0100

    fix serviceworker

--- static/serviceworker.updater.js
+++ static/serviceworker.updater.js
@@ -1,2 +1,2 @@
 importScripts('https://rocket.watch/external/notifications.js');
-importScripts('https://rocket.watch/js/serviceworker.min.js');
+importScripts('https://rocket.watch/js/serviceworker.js');
