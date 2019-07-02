b0e507cdfcec9106586693d3daa4b31583907c26
yasiupl <contact@yasiu.pl>
2019-07-02 19:34:54 +0200

    apiRouter fix for default response

--- backend/routers/apiRouter.js
+++ backend/routers/apiRouter.js
@@ -3,27 +3,27 @@ var Router = express.Router();
 const RocketWatch = require("../server.js");
 
 Router.route("/api/*").get((req, res) => {
-    if (req.url.split("api/")[1]) {
-      RocketWatch.load(req.url.split("api/")[1], function (d) {
-        res.set({
-          'Access-Control-Allow-Origin': '*',
-          'Cache-Control': 'max-age=' + Math.round((d.expire - Date.now()) / 1000)
-        });
-        res.json(d);
-      });
-    } else {
-      res.json({
-        endpoints: {
-          launch: "https://rocket.watch/api/launch",
-          mission: "https://rocket.watch/api/mission",
-          rocket: "https://rocket.watch/api/rocket",
-          agency: "https://rocket.watch/api/agency"
-        },
-        docs: "https://launchlibrary.net/docs/1.4/api.html",
-        info: "https://launchlibrary.net",
-        version: "1.4.1"
-      })
-    }
+  RocketWatch.load(req.url.split("api/")[1], function (d) {
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'max-age=' + Math.round((d.expire - Date.now()) / 1000)
+    });
+    res.json(d);
   });
+});
+
+Router.route("/api/").get((req, res) => {
+  res.json({
+    endpoints: {
+      launch: "https://rocket.watch/api/launch",
+      mission: "https://rocket.watch/api/mission",
+      rocket: "https://rocket.watch/api/rocket",
+      agency: "https://rocket.watch/api/agency"
+    },
+    docs: "https://launchlibrary.net/docs/1.4/api.html",
+    info: "https://launchlibrary.net",
+    version: "1.4.1"
+  })
+});
 
 module.exports = Router;
