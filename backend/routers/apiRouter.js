10753b2753b5abec16defae9067a63f95b9d5185
yasiupl <contact@yasiu.pl>
2019-07-03 14:20:13 +0200

    improved path detection on /api endpoint

--- backend/routers/apiRouter.js
+++ backend/routers/apiRouter.js
@@ -2,16 +2,6 @@ var express = require('express');
 var Router = express.Router();
 const RocketWatch = require("../server.js");
 
-Router.route("/api/*").get((req, res) => {
-  RocketWatch.load(req.url.split("api/")[1], function (d) {
-    res.set({
-      'Access-Control-Allow-Origin': '*',
-      'Cache-Control': 'max-age=' + Math.round((d.expire - Date.now()) / 1000)
-    });
-    res.json(d);
-  });
-});
-
 Router.route("/api/").get((req, res) => {
   res.json({
     endpoints: {
@@ -24,6 +14,16 @@ Router.route("/api/").get((req, res) => {
     info: "https://launchlibrary.net",
     version: "1.4.1"
   })
+});
+
+Router.route("/api/*").get((req, res) => {
+  RocketWatch.load(req.path.split("/api/")[1], function (d) {
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'max-age=' + Math.round((d.expire - Date.now()) / 1000)
+    });
+    res.json(d);
+  });
 });
 
 module.exports = Router;
