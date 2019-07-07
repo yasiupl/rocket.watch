50b6af4c2cea90936ec3f44439916b8e6e03e1ab
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-07-07 17:10:49 +0200

    Add basic unit testing machinery
    Also: added a sample tests + npm audit

---
+++ backend/jest.config.js
@@ -0,0 +1,16 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    testEnvironment: "node",
+    errorOnDeprecated: true,
+    resetModules: true,
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.js"],
+    coverageDirectory: "coverage",
+    coverageReporters: [
+      "text",
+      "lcov",
+    ],
+};
---
+++ backend/tests/unit/middlewares/defaultHeaders.test.js
@@ -0,0 +1,29 @@
+const httpMocks = require('node-mocks-http');
+
+const defaultHeaders = require("../../../src/middlewares/defaultHeaders");
+
+
+test('calls next()', () => {
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    defaultHeaders(null, response, next)
+
+    expect(next).toHaveBeenCalled();
+});
+
+test('applies default headers', () => {
+    const response = httpMocks.createResponse();
+    const next = () => null;
+
+    defaultHeaders(null, response, next);
+
+    expect(response._headers).toEqual({
+        "strict-transport-security": "max-age=15768000; includeSubDomains; preload",
+        "referrer-policy": "same-origin",
+        "x-content-type-options": "nosniff",
+        "x-frame-options": "SAMEORIGIN",
+        "x-xss-protection": "1",
+        "cache-control": "max-age=2592000",
+    });
+});
