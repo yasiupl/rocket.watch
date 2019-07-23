37e3efa57d4aefc4be279bf7e791879317160ab7
yasiupl <contact@yasiu.pl>
2019-07-23 18:46:00 +0200

    lint source code

--- backend/src/scrape/agencyType.js
+++ backend/src/scrape/agencyType.js
@@ -4,12 +4,15 @@ const agenciesType = [
     "Commercial",
     "Educational",
     "Private",
-  ];
+];
 
-function agencyType(agencyID, agenciesResolutionTable = agenciesType) {
-  if(agencyID > 0 && agencyID - 1 < agenciesResolutionTable.length) {
-    return agenciesResolutionTable[agencyID - 1];
-  } else return "Unknown"
+function agencyType(agencyID, agenciesResolutionTable = agenciesType) 
+{
+    if(agencyID > 0 && agencyID - 1 < agenciesResolutionTable.length) 
+    {
+        return agenciesResolutionTable[agencyID - 1];
+    }
+    else return "Unknown";
 }
 
 module.exports = agencyType;
