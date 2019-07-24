a9b7426d8450754945d3010846e97b76c659c4ee
yasiupl <contact@yasiu.pl>
2019-07-24 02:03:44 +0200

    additional test for empy reference table entries

--- backend/tests/unit/scrape/agencyShortName.test.js
+++ backend/tests/unit/scrape/agencyShortName.test.js
@@ -6,6 +6,7 @@ const stub = [
     "stub",
     "test",
     "testing",
+    "",
 ]
 
 each([undefined, null]).test('return "Unknown" on undefined/null', (agencyID) => {
@@ -31,4 +32,12 @@ each([1,2,3]).test('returns valid agency from agencyResolutionTable', (agencyID)
     const result = agencyType(agencyID, stub)
 
     expect(result).toBe(stub[agencyID-1]);
+});
+
+each([4]).test('return "Unknown" when entry is empty in the reference table', (agencyID) => {
+   
+    agencyID
+    const result = agencyType(agencyID, stub)
+
+    expect(result).toBe('Unknown');
 });
