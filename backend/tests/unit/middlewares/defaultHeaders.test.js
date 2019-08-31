48a82d3eb50f1bda54df4821f4b6f76a94bf8d59
yasiupl <contact@yasiu.pl>
2019-09-01 01:59:33 +0200

    tabs to spaces

--- backend/tests/unit/middlewares/defaultHeaders.test.js
+++ backend/tests/unit/middlewares/defaultHeaders.test.js
@@ -1,29 +1,31 @@
-const httpMocks = require('node-mocks-http');
+const httpMocks = require("node-mocks-http");
 
 const defaultHeaders = require("middlewares/defaultHeaders");
 
 
-test('calls next()', () => {
-    const response = httpMocks.createResponse();
-    const next = jest.fn();
+test("calls next()", () => 
+{
+	const response = httpMocks.createResponse();
+	const next = jest.fn();
 
-    defaultHeaders(null, response, next)
+	defaultHeaders(null, response, next);
 
-    expect(next).toHaveBeenCalled();
+	expect(next).toHaveBeenCalled();
 });
 
-test('applies default headers', () => {
-    const response = httpMocks.createResponse();
-    const next = () => null;
+test("applies default headers", () => 
+{
+	const response = httpMocks.createResponse();
+	const next = () => null;
 
-    defaultHeaders(null, response, next);
+	defaultHeaders(null, response, next);
 
-    expect(response._headers).toEqual({
-        "strict-transport-security": "max-age=15768000; includeSubDomains; preload",
-        "referrer-policy": "same-origin",
-        "x-content-type-options": "nosniff",
-        "x-frame-options": "SAMEORIGIN",
-        "x-xss-protection": "1",
-        "cache-control": "max-age=2592000",
-    });
+	expect(response._headers).toEqual({
+		"strict-transport-security": "max-age=15768000; includeSubDomains; preload",
+		"referrer-policy": "same-origin",
+		"x-content-type-options": "nosniff",
+		"x-frame-options": "SAMEORIGIN",
+		"x-xss-protection": "1",
+		"cache-control": "max-age=2592000",
+	});
 });
--- backend/tests/unit/scrape/agencyShortName.test.js
+++ backend/tests/unit/scrape/agencyShortName.test.js
@@ -1,43 +1,47 @@
-const each = require('jest-each').default;
+const each = require("jest-each").default;
 
 const agencyType = require("scrape/agencyShortName");
 
 const stub = {
-    1: "stub",
-    2: "test",
-    3: "testing",
-    4: "",
-}
+	1: "stub",
+	2: "test",
+	3: "testing",
+	4: "",
+};
 
-each([undefined, null]).test('return "Unknown" on undefined/null', (agencyID) => {
+each([undefined, null]).test("return \"Unknown\" on undefined/null", (agencyID) => 
+{
    
 
-    const result = agencyType(agencyID, stub)
+	const result = agencyType(agencyID, stub);
 
-    expect(result).toBe('Unknown');
+	expect(result).toBe("Unknown");
 });
 
 
-each([-100, 0, 4, 100]).test('return "Unknown" on value outside reference table', (agencyID) => {
+each([-100, 0, 4, 100]).test("return \"Unknown\" on value outside reference table", (agencyID) => 
+{
    
 
-    const result = agencyType(agencyID, stub)
+	const result = agencyType(agencyID, stub);
 
-    expect(result).toBe('Unknown');
+	expect(result).toBe("Unknown");
 });
 
-each([[1,"stub"],[2, "test"],[3,"testing"]]).test('returns valid agency from agencyResolutionTable', (agencyID, name) => {
+each([[1,"stub"],[2, "test"],[3,"testing"]]).test("returns valid agency from agencyResolutionTable", (agencyID, name) => 
+{
    
 
-    const result = agencyType(agencyID, stub)
+	const result = agencyType(agencyID, stub);
 
-    expect(result).toBe(name);
+	expect(result).toBe(name);
 });
 
-each([4]).test('return "Unknown" when entry is empty in the reference table', (agencyID) => {
+each([4]).test("return \"Unknown\" when entry is empty in the reference table", (agencyID) => 
+{
    
-    agencyID
-    const result = agencyType(agencyID, stub)
+	agencyID;
+	const result = agencyType(agencyID, stub);
 
-    expect(result).toBe('Unknown');
+	expect(result).toBe("Unknown");
 });
