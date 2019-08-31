39673297032d47a7f8c87071a92e3b93ffd9bfc4
yasiupl <contact@yasiu.pl>
2019-08-31 23:13:59 +0200

    fix #26

--- backend/tests/e2e/smoke-test.sh
+++ backend/tests/e2e/smoke-test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+node index.js 2>&1 >/dev/null &
+sleep 2
 curl --silent http://localhost:9000 2>&1 >/dev/null
 CURL_EXIT_CODE=$?
+sleep 2
 pkill node
+echo $CURL_EXIT_CODE
 exit $CURL_EXIT_CODE
