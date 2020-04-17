b185940a9c653dbccbb7280e8e0606ab04bc89a1
yasiupl <contact@yasiu.pl>
2020-04-17 12:49:31 +0200

    split backend into separate repository

--- backend/tests/e2e/smoke-test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-node index.js 2>&1 >/dev/null &
-sleep 2
-curl --silent http://localhost:9000 2>&1 >/dev/null
-CURL_EXIT_CODE=$?
-sleep 2
-pkill node
-echo $CURL_EXIT_CODE
-exit $CURL_EXIT_CODE
