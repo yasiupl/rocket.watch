8fabe38b83b0915a7dd98eae7979bee4846dd99d
Marcin Jasiukowicz <contact@yasiu.pl>
2020-11-18 21:55:05 +0100

    tweak service worker cache

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,18 +1,33 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
-
 workbox.googleAnalytics.initialize();
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-
 workbox.routing.registerRoute(
-  new RegExp(/.*/),
+  ({url}) => url.origin === 'https://ll.thespacedevs.com',
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'rocketwatch-cache',
+    cacheName: 'll-api-cache',
     plugins: [
       new workbox.expiration.Plugin({
         maxAgeSeconds: 24 * 60 * 60,
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
+
+
+workbox.routing.registerRoute(
+  ({url}) => (url.origin === 'https://rocket.watch' &&
+  url.pathname.startsWith('/assets/')) || (url.origin === 'https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60,
       }),
     ],
   })
